4e91171f57d06b6f582968fc85a68b4940a8d22c
davidco27 <74071922+davidco27@users.noreply.github.com>
2021-04-05 22:28:55 +0200

    Add files via upload

--- teamschedule.js
+++ teamschedule.js
@@ -22,10 +22,10 @@ function getSchedule(){
        var fecha= game.scheduled.substring(0,game.scheduled.indexOf('T')).split('-');
           var linea= '<div class="card d-inline-block" style="width: 30%;margin-left: 2%; margin-bottom: 1%;">'+
           '<div class="card-header">'+ fecha[2]+'-'+fecha[1]+'-'+fecha[0]+" "+game.venue.name+", "+ game.venue.city+'</div><div class="card-body"><div class="local">'
-          +'<img class "d-inline-block" width=15% height=10% src="https://loodibee.com/wp-content/uploads/nfl-'+local+'-team-logo-2-350x350.png">'
+          +'<img class "d-inline-block" width=15% height=10% src="//loodibee.com/wp-content/uploads/nfl-'+local+'-team-logo-2-350x350.png">'
           +'<p class="d-inline-block card-text"><strong>'+ game.home.name+'</strong></p>'
           +'<p  style="margin-top: 5%;" class="d-inline-block float-right">'+game.scoring.home_points+'</p></div><div class="visitante">'
-          +'<img class "d-inline-block" width=15% height=10% src="https://loodibee.com/wp-content/uploads/nfl-'+visitante+'-team-logo-2-350x350.png"><p class="d-inline-block card-text"><strong>'+ game.away.name+'</strong></p>'
+          +'<img class "d-inline-block" width=15% height=10% src="//loodibee.com/wp-content/uploads/nfl-'+visitante+'-team-logo-2-350x350.png"><p class="d-inline-block card-text"><strong>'+ game.away.name+'</strong></p>'
           +'<p  style="margin-top: 5%;" class="d-inline-block float-right">'+game.scoring.away_points+'</p></div>'
           +'<a class="stretched-link" href="stats.html?gameId='+game.id+'"></a></div></div>';
           html=html+linea;
